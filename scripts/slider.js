7f98b319582b03a540781392f81ed1ecf88f8c88
Alberto <albertogilsanjuan@gmail.com>
2025-01-09 17:38:38 +0100

    ADD: Home Page

--- scripts/slider.js
+++ scripts/slider.js
@@ -22,10 +22,42 @@ const facilitiesSwiper = new Swiper('.facilities__swiper', {
     }
 })
 
-const fcounterSwiper = new Swiper('.counter__swiper', {
+const counterSwiper = new Swiper('.counter__swiper', {
     loop: true,
     pagination: {
         el: '.counter__swiper .swiper-pagination',
+        clickable: true
+    }
+})
+
+const homeRoomsSwiper = new Swiper('.homeRooms__swiper', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+})
+
+const facilitiesHomeSwiper = new Swiper('.facilitiesHome__swiper', {
+    loop: true,
+    pagination: {
+        el: '.facilitiesHome__swiper .swiper-pagination',
+        clickable: true
+    }
+})
+
+const menuHomeSwiper = new Swiper('.menu__swiper', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+})
+
+const menuSwiper = new Swiper('.menu__swiper2', {
+    loop: true,
+    pagination: {
+        el: '.menu__swiper2 .swiper-pagination',
         clickable: true
     }
 })
