832dbee87416e4215ea12063d3183809405644fe
Alberto <albertogilsanjuan@gmail.com>
2025-01-09 12:18:33 +0100

    ADD: AboutUs Page

--- scripts/slider.js
+++ scripts/slider.js
@@ -14,3 +14,18 @@ const relatedRoomsSwiper = new Swiper('.relatedRooms__swiper', {
     }
 })
 
+const facilitiesSwiper = new Swiper('.facilities__swiper', {
+    loop: true,
+    pagination: {
+        el: '.facilities__swiper .swiper-pagination',
+        clickable: true
+    }
+})
+
+const fcounterSwiper = new Swiper('.counter__swiper', {
+    loop: true,
+    pagination: {
+        el: '.counter__swiper .swiper-pagination',
+        clickable: true
+    }
+})
